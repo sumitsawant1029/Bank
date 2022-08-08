c83c48cbb58b9acb39272d97d01a9df9a5e9cf6c
sumitsawant1029 <110157516+sumitsawant1029@users.noreply.github.com>
2022-08-08 11:58:21 -0700

    Added Exit and Interest

--- bank.java
+++ bank.java
@@ -2,23 +2,25 @@ import java.util.Scanner;
 public class bank{
     public static void main(String arg[]){
         // For taking Inputs 
-        System.out.println("Enter Your Name : ");
+        System.out.print("Enter Your Name : ");
         Scanner obj=new Scanner(System.in);
         String a = obj.nextLine();
-        System.out.println("Enter Your Adderess : ");
+        System.out.print("Enter Your Adderess : ");
         String e  = obj.nextLine();
-         System.out.println("Enter Your Account Number : ");
+         System.out.print("Enter Your Account Number : ");
         long b = obj.nextLong();
-         System.out.println("Enter Your Initial Balance : ");
+         System.out.print("Enter Your Initial Balance : ");
         double c = obj.nextDouble();
-         System.out.println("Enter Your Rate Of Interest : ");
+         System.out.print("Enter Your Rate Of Interest : ");
         int d = obj.nextInt();
 
-        System.out.println("Enter What Type Of Transaction You Want to Proceed \n");
-        System.out.println("1: Create Account \n2) Deposit Cash \n3) Withdraw Cash\n4) Compute Yearly Interest\n5) Display Balance");
-        System.out.println("Enter Your Choice :");
-        int f= obj.nextInt();
         
+        int w=0;
+       while (w<1){
+        System.out.println("\n\n\nEnter What Type Of Transaction You Want to Proceed \n");
+        System.out.println("1: Create Account \n2) Deposit Cash \n3) Withdraw Cash\n4) Compute Yearly Interest\n5) Display Balance \n6) Exit");
+        System.out.println("Enter Your Choice :");
+        int f= obj.nextInt(); 
         switch(f){
             case 1 : 
             break ;
@@ -41,12 +43,20 @@ public class bank{
                 System.out.println("Your Total Balance is "+x3);
                 break;
             case 4 :
-                
+                double v = c * d / 100;
+                System.out.println(" Your Yearly Interest is "+v);
+                break;
             case 5 :
-                System.out.println("Your Total Balance is "+c);    
+                System.out.println("Your Total Balance is "+c);
+                break;
+            case 6 :
+                w=1;
+                break;
+            default :
+                System.out.println("Your Entered a Wrong Choice") ;   
         }
          
-
+       }
     }
     public static double create(){
         Scanner obj=new Scanner(System.in);
